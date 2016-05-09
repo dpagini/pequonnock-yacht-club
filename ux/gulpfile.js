7cf545a0644fb100d57ea815017b60b203d0931b
David Pagini <davidpagini+github@gmail.com>
2016-05-09 17:35:59 -0400

    install gulp-sass

---
+++ ux/gulpfile.js
@@ -0,0 +1,8 @@
+// TODO: can this file stay here? not in the root?
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
